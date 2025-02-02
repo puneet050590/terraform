7181f4841d64a8dbe24a5e90a1f7406e64245103
puneet050590 <puneet050590@gmail.com>
2025-02-02 22:57:56 +0530

    v1.3

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.84.0"
-    }
-        }
-  }
-
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-}
-
 # Create a VPC
 resource "aws_vpc" "test01" {
   cidr_block = "192.168.1.0/24"  
