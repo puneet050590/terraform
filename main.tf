c6583a4ac0492f7a8acb59297c703aed641ed557
puneet050590 <puneet050590@gmail.com>
2025-02-02 22:56:41 +0530

    v1.3

--- main.tf
+++ main.tf
@@ -4,39 +4,15 @@ terraform {
       source = "hashicorp/aws"
       version = "5.84.0"
     }
-    google = {
-      source = "hashicorp/google"
-      version = "6.17.0"
-    }
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.16.0"
-    }
+        }
   }
-}
 
 provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
-provider "azurerm" {
-  # Configuration options
-  features {}
-  subscription_id = "dc8b8ab6-3e48-4de2-bd1f-0feeb24409c8"
-}
-provider "google" {
-  # Configuration options
-  project = "Terraform"
-} 
+
 # Create a VPC
 resource "aws_vpc" "test01" {
   cidr_block = "192.168.1.0/24"  
-}
-# Create a resource group
-#resource "azurerm_resource_group" "testTF01" {
-#  name     = "example-resources"
-#  location = "West Europe"
-#}
-resource "google_compute_network" "vpc_network" {
-  name = "vpc-network"
 }
---
+++ provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.84.0"
+    }
+    google = {
+      source = "hashicorp/google"
+      version = "6.17.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+        }
+  }
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "dc8b8ab6-3e48-4de2-bd1f-0feeb24409c8"
+}
+provider "google" {
+  # Configuration options
+  project = "Terraform"
+} 
